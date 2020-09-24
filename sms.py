ea54f6e76263f1a7d0d57cce44c83ba3b3be6501
CedarSky <43072877+ChrissyHandley@users.noreply.github.com>
2020-09-24 17:28:11 -0400

    Security

--- sms.py
+++ sms.py
@@ -1,8 +1,8 @@
 from twilio.rest import Client
 
 
-account_sid = 'AC83b951475648b98859265d4ec686b1cd'
-auth_token = 'b20592bdebf3ceff1581328b4ee381fd'
+account_sid = '........'
+auth_token = '.........'
 client = Client(account_sid, auth_token)
 
 message = client.messages \
